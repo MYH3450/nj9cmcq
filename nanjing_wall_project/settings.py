eac4e07d940fa9d7adec665a7de1961604cfa59c
mer3450 <404261479@qq.com>
2026-04-11 15:05:16 +0800

    wufgug

--- nanjing_wall_project/settings.py
+++ nanjing_wall_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-clr$lbhrna6twfp#vwv9^@61x84u$#3*md@_*n1ajpsc2vw^sp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["nj9cmcq1.up.railway.app", "localhost", "127.0.0.1","nj9cmcq.up.railway.app"]
 
